d56592a24941896a170c080160fd1e3585d7101a
wicked777 <wicky.akloe@gmail.com>
2019-11-11 12:36:54 +0100

    Change name from HE-Ware to HI-Ware

--- hiware/wsgi.py
+++ hiware/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for heware project.
+WSGI config for hiware project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'heware.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hiware.settings')
 
 application = get_wsgi_application()
