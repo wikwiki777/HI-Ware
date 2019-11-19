22bebaa035f5bfb16ceadc0427bed1896764bb5e
wicked777 <wicky.akloe@gmail.com>
2019-11-19 18:42:49 +0100

    Create login test and view

--- hiware/urls.py
+++ hiware/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from accounts import views
+from django.urls import path, include
+from accounts import urls as accounts_urls
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('accounts/', include(accounts_urls)),
 ]
