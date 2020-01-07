09567b0b3a44eb14b96e43e803b588f106888aff
wicked777 <wicky.akloe@gmail.com>
2020-01-07 17:08:57 +0100

    Add stripe js

--- checkout/views.py
+++ checkout/views.py
@@ -12,7 +12,7 @@ import stripe
 stripe.api_key = settings.STRIPE_SECRET
 
 
-@login_required(login_url="/accounts/login/")
+@login_required(login_url="/accounts/login")
 def checkout(request):
     if request.method == "POST":
         order_form = OrderForm(request.POST)
@@ -46,7 +46,7 @@ def checkout(request):
                 messages.error(request, "Your card was declined!")
 
             if customer.paid:
-                messages.error(request, "You have successfully paid")
+                messages.success(request, "You have successfully paid")
                 request.session['cart'] = {}
                 return redirect(reverse('products'))
             else:
